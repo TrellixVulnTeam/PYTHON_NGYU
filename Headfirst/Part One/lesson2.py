9e1c3fe30ed2d6474afd54f8a139d86cf07ed0bb
i.nikolayev <i.nikolayev@epc-it.by>
2019-07-23 11:32:18 +0300

    #comment lesson 2,3

--- Headfirst/Part One/lesson2.py
+++ Headfirst/Part One/lesson2.py
@@ -6,10 +6,10 @@ condition = datetime.today().day # today().day
 
 if today == 'Saturday':  # если день Saturday
     print('Party!') # вывод истины
-elif today == 'Sunday': # вывод истины
+elif today == 'Sunday': # или если sunday
     if condition == 'Headache':
         print('Recover, then rest.')
     else:
         print('Rest.')
 else:
-    print('Work, Work, Work')
+    print('Work, Work, Work') # вывод если условие ложно
